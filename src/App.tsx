fc1ec080a69555bf8b6d6233b690500ed9a759ef
gurhanpro <ziyaadyare@gmail.com>
2025-07-27 12:06:05 +0200

    add a basic search input

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,23 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [searchTerm, setSearchTerm] = useState('')  
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value)
+    console.log(event.target.value)
+  }
 
   return (
     <div>
       <h1>Star Wars Characters</h1>
+      <input
+        type="text"
+        placeholder="Search characters..."
+        onChange={handleSearchChange}
+      />
     </div>
   )
 }
-
 export default App
